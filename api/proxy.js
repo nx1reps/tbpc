82c4bf070e74f3d5a0bff5f491f92e45f0ecde29
NX1 <159203857+nx1reps@users.noreply.github.com>
2026-02-07 19:43:06 -0500

    Add proxy handler to fetch and return upstream data

---
+++ api/proxy.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  try {
+    const upstream = await fetch("http://65.109.112.222");
+    const body = await upstream.text();
+
+    res.setHeader("X-Frame-Options", "ALLOWALL");
+    res.setHeader("Content-Security-Policy", "frame-ancestors *");
+
+    res.status(200).send(body);
+  } catch (err) {
+    res.status(500).send("Proxy error");
+  }
+}
