dc738ff1d3c9a25a43bacf7df24041d6308c17c9
NX1 <159203857+nx1reps@users.noreply.github.com>
2026-02-07 19:50:18 -0500

    Change upstream URL from s0lace to tiktok.com

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const upstream = await fetch("https://s0lace.fly.dev");
+    const upstream = await fetch("tiktok.com");
     const body = await upstream.text();
 
     res.setHeader("X-Frame-Options", "ALLOWALL");
